e1a084107e11ae971768dd2ac7b2f7d97e88da8c
ChadxScript <aroloroso+github@gmail.com>
2023-06-28 21:00:14 +0800

    FIX something idunno

--- GRP3_Threading_Main.py
+++ GRP3_Threading_Main.py
@@ -45,38 +45,35 @@ class SingleThreaded:
 
     def run(self):
         print("\n\n---STARTING SINGLE-THREADED SYSTEM---")
-        while running:
-            # Fibonacci
-            n = random.randint(0, 20)
-            try:
-                # fib_num = self.fib.run(n)
-                fib_num = [self.fib.run(i) for i in range(n + 1)]
-                print("SINGLE-THREADED: n: ", n)
-                print("SINGLE-THREADED: FIBONACCI NUMBER IS: ", fib_num, "\n")
-            except ValueError as e:
-                print("\nERROR: ", e)
+        # Fibonacci
+        n = random.randint(0, 20)
+        try:
+            # fib_num = self.fib.run(n)
+            fib_num = [self.fib.run(i) for i in range(n + 1)]
+            print("[SINGLE-THREADED]\t n: ", n)
+            print("[SINGLE-THREADED]\t FIBONACCI NUMBER IS: ", fib_num, "\n")
+        except ValueError as e:
+            print("\nERROR: ", e)
 
-            # Square
-            try:
-                sqr_num = self.sqr.run(n)
-                print("SINGLE-THREADED: n: ", n)
-                print("SINGLE-THREADED: SQUARED NUMBER IS: [", sqr_num, "]\n")
-            except ValueError as e:
-                print("\nERROR: ", e)
+        # Square
+        try:
+            sqr_num = self.sqr.run(n)
+            print("[SINGLE-THREADED]\t n: ", n)
+            print("[SINGLE-THREADED]\t SQUARED NUMBER IS: [", sqr_num, "]\n")
+        except ValueError as e:
+            print("\nERROR: ", e)
 
-            # SortNumber
-            sort_num = []
-            for _ in range(5):
-                sort_num.append(random.randint(0, 100))
+        # SortNumber
+        sort_num = []
+        for _ in range(5):
+            sort_num.append(random.randint(0, 100))
 
-            try:
-                sorted_num = self.sort.run(sort_num)
-                print("SINGLE-THREADED: UNSORTED NUMBERS: ", sort_num)
-                print("SINGLE-THREADED: SORTED NUMBERS: ", sorted_num, "\n")
-            except ValueError as e:
-                print("\nERROR: ", e)
-
-            time.sleep(1)
+        try:
+            sorted_num = self.sort.run(sort_num)
+            print("[SINGLE-THREADED]\t UNSORTED NUMBERS: ", sort_num)
+            print("[SINGLE-THREADED]\t SORTED NUMBERS: ", sorted_num, "\n")
+        except ValueError as e:
+            print("\nERROR: ", e)
 
 
 class MultiThreaded:
@@ -87,65 +84,68 @@ class MultiThreaded:
 
     def run(self):
         print("\n\n---STARTING MULTI-THREADED SYSTEM---")
-        while running:
-            threads = []
-            for i in range(5):
-                sort_num = []
-                for _ in range(5):
-                    sort_num.append(random.randint(0, 100))
-                n = random.randint(0, 20)
+        threads = []
+        for i in range(5):
+            sort_num = []
+            for _ in range(5):
+                sort_num.append(random.randint(0, 100))
+            n = random.randint(0, 20)
 
-                t1 = threading.Thread(target=self.print_fib, args=(n,))
-                t2 = threading.Thread(target=self.print_sqr, args=(n,))
-                t3 = threading.Thread(target=self.print_sort, args=(sort_num,))
+            t1 = threading.Thread(target=self.print_fib, args=(n,))
+            t2 = threading.Thread(target=self.print_sqr, args=(n,))
+            t3 = threading.Thread(target=self.print_sort, args=(sort_num,))
 
-                # Append the threads to the list
-                threads.append(t1)
-                threads.append(t2)
-                threads.append(t3)
+            # Append the threads to the list
+            threads.append(t1)
+            threads.append(t2)
+            threads.append(t3)
 
-                # Start the threads
-                t1.start()
-                t2.start()
-                t3.start()
+            # Start the threads
+            t1.start()
+            print("[MULTI-THREADED]\t THREAD [", t1.name, "] HAS PID OF : ", os.getpid())
+            print("[MULTI-THREADED]\t THREAD [ ", t1.name, " ] HAS THREAD ID OF : ", threading.get_ident())
+            t2.start()
+            print("[MULTI-THREADED]\t THREAD [ ", t2.name, " ] HAS PID OF : ", os.getpid())
+            print("[MULTI-THREADED]\t THREAD [ ", t2.name, " ] HAS THREAD ID OF : ", threading.get_ident())
+            t3.start()
+            print("[MULTI-THREADED]\t THREAD [ ", t3.name, " ] HAS PID OF : ", os.getpid())
+            print("[MULTI-THREADED]\t THREAD [ ", t3.name, " ] HAS THREAD ID OF : ", threading.get_ident())
 
-                # Print threads and PIDs
-                print("THREAD [ ", t1.name, " ] HAS PID OF : ", os.getpid())
-                print("THREAD [ ", t2.name, " ] HAS PID OF : ", os.getpid())
-                print("THREAD [ ", t3.name, " ] HAS PID OF : ", os.getpid())
+            t1.join()
+            t2.join()
+            t3.join()
 
             for t in threads:
                 t.join()
 
-            time.sleep(5)
-
     def print_fib(self, n):
         try:
-            # fib_num = self.fib.run(n)
             fib_num = [self.fib.run(i) for i in range(n + 1)]
-            print("MULTI-THREADED: n: ", n)
-            print("MULTI-THREADED: FIBONACCI NUMBER IS: ", fib_num, "\n")
+            print("[MULTI-THREADED]\t n: ", n)
+            print("[MULTI-THREADED]\t FIBONACCI NUMBER IS: ", fib_num, "\n")
+            time.sleep(1)
         except ValueError as e:
             print("\nERROR: ", e)
 
     def print_sqr(self, n):
         try:
             sqr_num = self.sqr.run(n)
-            print("MULTI-THREADED: n: ", n)
-            print("MULTI-THREADED: SQUARED NUMBER IS: [", sqr_num, "]\n")
+            print("[MULTI-THREADED]\t n: ", n)
+            print("[MULTI-THREADED]\t SQUARED NUMBER IS: [", sqr_num, "]\n")
+            time.sleep(2)
         except ValueError as e:
             print("ERROR: ", e)
 
     def print_sort(self, sort_num):
         try:
             sorted_num = self.sort.run(sort_num)
-            print("MULTI-THREADED: UNSORTED NUMBERS: ", sort_num)
-            print("MULTI-THREADED: SORTED NUMBERS: ", sorted_num, "\n")
+            print("[MULTI-THREADED]\t UNSORTED NUMBERS: ", sort_num)
+            print("[MULTI-THREADED]\t SORTED NUMBERS: ", sorted_num, "\n")
+            time.sleep(3)
         except ValueError as e:
             print("\nERROR: ", e)
 
 
-running = True
 fib = Fibonacci()
 sqr = Square()
 sort = SortNumbers()
@@ -154,19 +154,13 @@ single_threaded = SingleThreaded(fib, sqr, sort)
 multi_threaded = MultiThreaded(fib, sqr, sort)
 
 # Single-Threaded
-single_threaded_thread = threading.Thread(target=single_threaded.run)
-single_threaded_thread.start()
-print("SINGLE-THREADED SYSTEM PID: ", os.getpid())
+single_threaded = threading.Thread(target=single_threaded.run)
+single_threaded.start()
+print("[SINGLE-THREADED]\t SYSTEM PID: ", os.getpid())
+single_threaded.join()
 
 # Multi Threaded System
-multi_threaded_thread = threading.Thread(target=multi_threaded.run)
-multi_threaded_thread.start()
-print("MULTI-THREADED SYSTEM PID: ", os.getpid())
-
-# End of program
-input("\n\nPRESS ENTER KEY TO TERMINATE...\n")
-running = False
-
-single_threaded_thread.join()
-multi_threaded_thread.join()
-print("\n\nPROGRAM TERMINATED...")
+multi_threaded = threading.Thread(target=multi_threaded.run)
+multi_threaded.start()
+print("[MULTI-THREADED]\t SYSTEM PID: ", os.getpid())
+single_threaded.join()
